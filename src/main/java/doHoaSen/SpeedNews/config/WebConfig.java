aff699218c48c292d95674301b484e4b91a5c786
doHoaSen <applesusu999@gmail.com>
2025-09-04 18:19:31 +0900

    Update: update webconfig.java again

--- src/main/java/doHoaSen/SpeedNews/config/WebConfig.java
+++ src/main/java/doHoaSen/SpeedNews/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
         reg.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://fantastic-kashata-d47d49.netlify.app",
-                        "https://fantastic-kashata-d47d49--develop.netlify.app", // 브랜치 프리뷰
+                        "https://fantastic-kashata-d47d49.netlify.app",            // 프로덕션(네틀리파이 서브도메인)
+                        "https://develop--fantastic-kashata-d47d49.netlify.app",   // 브랜치 배포(정답)
                         "https://speednews.it.kr",
                         "https://www.speednews.it.kr"
                 )
